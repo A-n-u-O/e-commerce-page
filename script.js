d888a42a1ea6f51c26d308283793d9b4aa9c707e
A-n-u-O <anuvictor2005@gmail>
2024-02-15 13:50:50 +0100

    changes made

--- script.js
+++ script.js
@@ -9,9 +9,10 @@ document.addEventListener("DOMContentLoaded", function() {
     const displayCartContent = document.querySelector(".cart-content");
     const currentPrice = document.querySelector(".current-price");
     const addToCartBtn = document.querySelector(".adding-To-Cart");
-    let cartItemCount = 0;
-    updateCartIcon();
-
+    let shoppedItems = JSON.parse(localStorage.getItem("items")) || [];
+    // let cartItemCount = 0;
+     updateCartIcon(shoppedItems[0]?.quantity || 0);
+            displayCartItems(shoppedItems);
     //to change image to another image when thumbnail is clicked
     thumbnail.forEach(element => {
         element.addEventListener("click", function(e){
@@ -97,6 +98,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 deleteCartItems();
             });
         }
+        // cartItemsContainer.appendChild(checkoutButton);
     }
 
 
@@ -130,9 +132,9 @@ document.addEventListener("DOMContentLoaded", function() {
             // Update local storage with the new array of items
             localStorage.setItem("items", JSON.stringify(shoppedItems));
 
+            console.log(shoppedItems);
             // Update the cart icon
-            cartItemCount+= quantity;
-            updateCartIcon(cartItemCount);
+            updateCartIcon(shoppedItems[0]?.quantity || 0);
 
             // Display items in the cart
             displayCartItems(shoppedItems);
@@ -141,7 +143,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    function updateCartIcon() {
+    function updateCartIcon(shoppedItems) {
         // Get the cart icon element
         const cartIcon = document.querySelector(".cart");
     
@@ -150,7 +152,7 @@ document.addEventListener("DOMContentLoaded", function() {
         <svg width="22" height="20" xmlns="http://www.w3.org/2000/svg">
             <path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" fill="#69707D" fill-rule="nonzero"/>
         </svg>
-        <span class="cart-item-count">${cartItemCount}</span>
+        <span class="cart-item-count">${shoppedItems}</span>
         `;
     }    
 
